4939b45d735dced9c90de6fd41e1e94d31e41e3a
Chris <Christopherjorge47@icloud.com>
2024-05-01 19:19:36 -0400

    Added second content section

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
   const title = ["Sartre's List", "Better-Dressed People"]
   const topNav = ["Women's", "Men's", "On the Street", "The Catwalk", "AdWatch", "About"]
   const contentSection1 = ['11/12/20', "On The Street in Brooklyn", "https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg","C","ray ipsum, dolor sit amet consectetur adipisicing elit. Repellat quidem deserunt nemo dignissimos fuga veniam inventore eligendi magnam aperiam odit mollitia cupiditate eveniet velit officiis magni aliquid, laborum dolor? Quasi? Veniam amet rerum ducimus est ea at neque alias. Temporibus perspiciatis at impedit voluptas aut voluptates facere totam aliquid debitis rerum, ex consequuntur, nulla eius quae corrupti optio atque ut. Repellendus molestiae doloribus recusandae, itaque voluptatibus placeat repellat deserunt amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed nulla eveniet doloribus neque optio odio qui! Id architecto impedit consequatur rem quaerat voluptatibus pariatur quas, quod quisquam quo ab molestiae."]
+  const contentSection2 = ["11/11/20", "Vintage In Vogue", "https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg", "S", "elfies sunt, dolor sit amet consectetur adipisicing elit. Repellat quidem deserunt nemo dignissimos fuga veniam inventore eligendi magnam aperiam odit mollitia cupiditate eveniet velit officiis magni aliquid, laborum dolor? Quasi? Veniam amet rerum ducimus est ea at neque alias. Temporibus perspiciatis at impedit voluptas aut voluptates facere totam aliquid debitis rerum, ex consequuntur, nulla eius quae corrupti optio atque ut. Repellendus molestiae doloribus recusandae, itaque voluptatibus placeat repellat deserunt amet eaque dignissimos, iste sapiente magnam doloremque. Fugit, cum, cupiditate aliquam, mollitia quisquam sed nulla eveniet doloribus neque optio odio qui! Id architecto impedit consequatur r"]
   return (
     <div className="App">
-    <Homepage title = {title} topNav = {topNav} contentSection1 = {contentSection1}/>
+    <Homepage title = {title} topNav = {topNav} contentSection1 = {contentSection1} contentSection2 = {contentSection2}/>
     </div>
   );
 }
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Heading from './Heading'
 import NavBar from './NavBar'
 import ContentSection from './ContentSection'
-function Homepage({title, topNav, contentSection1}) {
+function Homepage({title, topNav, contentSection1, contentSection2}) {
   return (
     <div>
         <Heading content = {title}/>
         <NavBar content = {topNav}/>
         <ContentSection content = {contentSection1}/>
+        <ContentSection content = {contentSection2}/>
+
 
     </div>
   )
